c35e8ccaeb7aab3766f4c8c52416d3d6bc884512
IvanRobles <ivanroblesmvp@gmail.com>
2022-05-13 20:26:03 -0700

    set up different views for parents

--- app/web/src/public/js/main.js
+++ app/web/src/public/js/main.js
@@ -4,23 +4,50 @@ import { EventBus } from "./eventBus.js";
 
 const CONSOLE_BOOL = true;
 
-// /**
-//  * Event handler function to change the theme
-//  */
-// function handleThemeBtnPressed() {
-//     // Obtains an array of all <link> elements. Select your element using indexing. 
-//     let theme = document.getElementById("theme");
-//     let theme_btn = document.getElementById("theme-btn");
+/**
+ * Event handler function to change the table displayed to calendar view
+ */
+function showCalendarView() {
+    if (CONSOLE_BOOL) {
+        console.log("main.js - showCalendarView")
+    }
+    let o_cal_view_btn = document.getElementById("cal-view-btn");
+    // if not already in calender view, display it
+    if (o_cal_view_btn.classList.contains("clicked-btn") != true) {
+      //make this button look clicked
+      o_cal_view_btn.classList.add("clicked-btn");
+      //make other button not clicked
+      let o_child_view_btn = document.getElementById("child-view-btn");
+      o_child_view_btn.classList.remove("clicked-btn");
+    } else {
+      if (CONSOLE_BOOL) {
+        console.log("Calendar View Already Displayed")
+      }
+    }
+}
 
-//     // Change the value of href attribute to change the css sheet.
-//     if (theme.getAttribute("href") == "./css/colors.css") {
-//         theme.setAttribute("href", "./css/colors2.css");
-//         theme_btn.setAttribute("title", "Simple Theme");
-//     } else {
-//         theme.setAttribute("href", "./css/colors.css");
-//         theme_btn.setAttribute("title", "Complex Theme");
-//     }
-// }
+/**
+ * Event handler function to change the table displayed to child view
+ */
+ function showChildView() {
+    if (CONSOLE_BOOL) {
+        console.log("main.js - showChildView")
+    }
+    // if not already on child view, display it
+    let o_child_view_btn = document.getElementById("child-view-btn");
+    if (o_child_view_btn.classList.contains("clicked-btn") != true) {
+      //make this button look clicked
+      o_child_view_btn.classList.add("clicked-btn");
+      //make other button not clicked
+      let o_cal_view_btn = document.getElementById("cal-view-btn");
+      o_cal_view_btn.classList.remove("clicked-btn");
+    } else {
+      if (CONSOLE_BOOL) {
+        console.log("Child View Already Displayed")
+      }
+    }
+
+}
 
 /**
  * Event handler function to show MenuList when menu button is pressed
@@ -45,9 +72,17 @@ document.addEventListener("DOMContentLoaded", () => {
     let o_menu_btn = document.getElementById("menu-btn");
     o_menu_btn.addEventListener("click", showMenuList);
 
-    // Code for showing / hiding MenuList functionality
+    // Code for showing / hiding Add Chore Box functionality
     let o_add_chore_btn = document.getElementById("add-chore-btn");
     o_add_chore_btn.addEventListener("click", showAddChore);
+
+    // Code for showing / hiding Calendar View
+    let o_cal_view_btn = document.getElementById("cal-view-btn");
+    o_cal_view_btn.addEventListener("click", showCalendarView);
+
+    // Code for showing / hiding Calendar View
+    let o_child_view_btn = document.getElementById("child-view-btn");
+    o_child_view_btn.addEventListener("click", showChildView);
 
     
     if (CONSOLE_BOOL) {
