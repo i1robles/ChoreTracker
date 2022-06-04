bf27f12bb2c29f9493d268061ab13a6c1937717e
IvanRobles <ivanroblesmvp@gmail.com>
2022-06-03 18:59:42 -0700

    initialized db

--- app/web/src/server.py
+++ app/web/src/server.py
@@ -1,6 +1,15 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import FileResponse
+from pyramid.renderers import render_to_response
+
+import mysql.connector as mysql
+import os
+
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+db_host = os.environ['MYSQL_HOST']
 
 def get_home(req):
   return FileResponse("pages/index.html")
@@ -12,6 +21,13 @@ def get_signup_page(req):
   return FileResponse("pages/signUp.html")
   
 def get_main_page(req):
+  # Connect to the database and retrieve the users
+  # db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+  # cursor = db.cursor()
+  # cursor.execute("select child_name, child_points from username_users;")
+  # names = cursor.fetchall()
+  # db.close()
+  # return render_to_response('pages/main.html', {'names': names}, request=req)
   return FileResponse("pages/main.html")
 
 if __name__ == '__main__':
